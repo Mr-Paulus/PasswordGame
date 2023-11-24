e587fb2d6b77f12f33431516dfb53c5470a8014a
Plofkip <paul-van-eldonk@hotmail.com>
2023-11-24 20:20:15 +0100

    add questions

--- PasswordGame.py
+++ PasswordGame.py
@@ -1,2 +1,35 @@
 #Dummy code extra tekst
 
+#1 Your password must be at least 5 characters.
+
+#2 Your password must include a number.
+
+#3 Your password must include an uppercase letter.
+
+#4 Your password must include a special character.
+
+#5 The digits in your password must add up to 25.
+
+#6 Your password must include a month of the year.
+
+#7 Your password must include a month of the year.
+
+#8 Your password must include one of our sponsors:
+
+#9 The roman numerals in your password should multiply to 35. 
+
+#10 Your password must include this CAPTCHA:
+
+#11 Your password must include today's Wordle answer.
+
+#12 Your password must include a two letter symbol from the periodic table.
+
+#13 Your password must include the current phase of the moon as an emoji.
+
+#14 Your password must include the name of this country.
+
+#15 Your password must include a leap year.
+
+#16 Your password must include the best move in algebraic chess notation.
+
+print ('\U0001F312')
